d58cd00330d43e05084252ffd41899367f08a539
kowa1ski <kowalski@gmx.es>
2017-09-02 21:00:34 +0200

    MENU EDITAR - Mola, le he hecho dos botones.

--- app/src/main/java/com/kovas1ki/android/p3db/EdicionActivity.java
+++ app/src/main/java/com/kovas1ki/android/p3db/EdicionActivity.java
@@ -2,12 +2,22 @@ package com.kovas1ki.android.p3db;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class EdicionActivity extends AppCompatActivity {
+
+    // Vamos a casar el nuevo menú
+    // Estoy adaptando del del MAin.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edicion);
     }
+
 }
