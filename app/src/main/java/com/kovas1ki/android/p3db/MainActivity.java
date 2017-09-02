3ee9147a6897fa75d9436c462c2b1fbc1a33e3c4
kowa1ski <kowalski@gmx.es>
2017-09-02 11:21:36 +0200

    RUN FIRST - Comprobado. Todo corre bien.

--- app/src/main/java/com/kovas1ki/android/p3db/MainActivity.java
+++ app/src/main/java/com/kovas1ki/android/p3db/MainActivity.java
@@ -18,11 +18,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Vamos aprobar que funciona reemplazando el mensaje de este button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "PROBANDO FUNCIONAMIENTO DEL BOTÓN", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
