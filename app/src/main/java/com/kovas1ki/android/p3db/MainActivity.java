1264cd85c9fa358d59ba1dc9e17b1c43fefca2ce
kowa1ski <kowalski@gmx.es>
2017-09-06 13:09:42 +0200

    LISTADO LoaderManager - Implementado virgen. En la línea 86 está el , getLoaderManager().initLoader(ITEMS_LOADER, null, this); , Ojo. Poner esa línea la primera antes de implementar nada. A partir de ese , this , generaremos de manera automática la implementación y luego sus métodos.

--- app/src/main/java/com/kovas1ki/android/p3db/MainActivity.java
+++ app/src/main/java/com/kovas1ki/android/p3db/MainActivity.java
@@ -1,16 +1,32 @@
 package com.kovas1ki.android.p3db;
 
+import android.app.LoaderManager;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+
+// NOOOOOOO . no implementamos nada. Desde el , this , de abajo lo forzamos y es más práctico. Eso sí
+// donde pone , <Objetct> , en automático hay que cambiarlo por la palabra , Cursor ,.
+// (y en los métodos que generamos a partir de esto también le cambiamos a , Cursor ,.
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    // Hemos creado la clase cursorAdapter para algo así que
+    // Declaramos el cursorAdapter y ponemos su
+    // parámetro de inicio a 0.
+    P3dbCursorAdapter p3dbCursorAdapter;
+    private static final int ITEMS_LOADER = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +47,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Buscamos la lista donde vamos a publicar los registros devueltos
+        // por el cursor
+        ListView itemListView = (ListView) findViewById(R.id.listViewDeRegistros);
+
+        // Arriba hemos declarado el adaptador pero no lo hemos iniciado.
+        // Vamos a coger esa variable y le vamos a decirle que cree un
+        // nuevo cursorAdapter.
+        // Cuidado!!!!, le pasamos el parámetro cursor en , null ,. Importante.
+        p3dbCursorAdapter = new P3dbCursorAdapter(this, null);
+        // Y ordenamos la adaptación de la lista
+        itemListView.setAdapter(p3dbCursorAdapter);
+
+        // Hacemos uso de la clase LoaderManager para iniciar la misma,
+        // que para eso la implementamos en la declaración de esta.
+        // Es aquí donde tenemos el , this , que dispara toda la secuencia
+        // de la implementación automática en el código de tod lo necesario
+        // con la implementeación de LoaderManager.
+        getLoaderManager().initLoader(ITEMS_LOADER, null, this);
+
+
     }
 
     @Override
@@ -54,4 +91,25 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    // Estos se generan automáticos desde arriba. Pero, como arriba cambiamos
+    // Object por Cursor, pues aquí también.
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
 }
+
+
+
